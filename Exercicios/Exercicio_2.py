583769537876eec484fa2b2291e20e546b527e22
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-09-11 20:35:02 -0300

    initial commit

---
+++ Exercicios/Exercicio_2.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    listNumbers: str = input("Please enter the list of numbers you want in ascending order, separated by spaces: ")
+    listNumbers = listNumbers.split()
+    result = [int(num) for num in listNumbers]
+    result.sort()
+        
+    print(f"The list of numbers in ascending order is: {result}")
+    
+if __name__ == "__main__":
+    main()
